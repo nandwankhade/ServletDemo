7962331401cfa29a54da8d9538b64f10a432f60f
nandwankhade <51238085+nandwankhade@users.noreply.github.com>
2019-06-04 16:09:49 +0530

    Feature 2 added

--- Demo/src/MainDemo.java
+++ Demo/src/MainDemo.java
@@ -4,6 +4,8 @@ public class MainDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Feature 1 :");
+		System.out.println("Feature 2 :");
+		
 	}
 
 }
